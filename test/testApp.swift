0d2f77486f58740ec0d2d7ce534937c968d3c84b
ezez20 <ezezyeoh@hotmail.com>
2023-05-20 16:08:24 -0500

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,20 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Ezra Yeoh on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
